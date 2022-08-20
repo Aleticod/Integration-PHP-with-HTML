1a5fca7375fac394cde3e34bb91b0f18ce7db42d
me <193837@unsaac.edu.pe>
2022-08-21 01:16:14 +0200

    Variables php passed to javaScript

---
+++ variable-js.php
@@ -0,0 +1,41 @@
+<?php 
+
+$users = array(
+    array(
+        'id' => 0,
+        'username' => 'Retax'
+    ),
+    array(
+        'id' => 1,
+        'username' => 'Juan'
+    ),
+    array(
+        'id' => 2,
+        'username' => 'Maria'
+    ),
+);
+
+$edad = 12;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1>Pasar variables a java script</h1>
+
+    <script>
+        let user = JSON.parse('<?= json_encode($users) ?>');
+        let edad = <?= $edad ?>;
+    </script>
+    <script src = "./variabless.js"></script>
+</body>
+
+</html>
