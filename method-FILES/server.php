13bfcfe15f2b6d3988b99b86eeaf8b49058cda19
me <193837@unsaac.edu.pe>
2022-08-21 07:21:16 +0200

    The server was modified to storage image into a ower directory to save images

--- method-FILES/server.php
+++ method-FILES/server.php
@@ -4,6 +4,10 @@ echo "<pre>";
 var_dump($_FILES);
 
 $basename = $_FILES["imagen"]["name"];
+$image_path = $_FILES["imagen"]["tmp_name"];
+$path_save = "images/$basename";
 
-echo $basename;
+move_uploaded_file($image_path, $path_save);
+
+
 ?>
