9358d1a3c15ae1ab9b9c5f1577264f0914b99d22
me <193837@unsaac.edu.pe>
2022-08-21 04:53:13 +0200

    Send information POST GET REQUEST server directory

---
+++ server/server.php
@@ -0,0 +1,7 @@
+<?php 
+// Variable super global llamda POST
+var_dump($_POST); // Recive en un array las variables enviadas por post
+var_dump($_GET); // Recive en un array las variables enviadas pro get
+var_dump($_REQUEST); // Recive en un array las variables enviadas por get y post
+
+?>
